f1ce05728746a44ee3cf26a61bea82923c3d8d4e
Andrew Grange <andrewgrange@workfront.com>
2018-12-11 23:26:28 -0700

    all vendors

--- hpd3/src/routes.php
+++ hpd3/src/routes.php
@@ -5,10 +5,27 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $this->renderer->render($response, 'login.phtml', $args);
 });
+
+$app->get('/client', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'client.phtml', $args);
+});
+
+$app->get('/admin', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'admin.phtml', $args);
+});
+
